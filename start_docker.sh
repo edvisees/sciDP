241cf00d128d8ea8e63bc9ced118b78650fed16d
GullyAPCBurns <GullyBurns@gmail.com>
2018-03-01 22:05:12 -0800

    Minor updates to docker scripts

--- start_docker.sh
+++ start_docker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 if [[ $# -ne 4 ]] ; then
-  echo "USAGE ./run_docker.sh <JUPYTER_PORT> <SCIDT_PORT> <DATA_FOLDER> <ES_DATA>"
+  echo "Run this from wihin the data directory you are analyzing"
+  echo "USAGE ./run_docker.sh <JUPYTER_PORT> <SCIDT_PORT> <ES_DATA> <SCIDT_FOLDER>"
   exit
 fi
 
 JPORT=$1
 SPORT=$2
 ES_DATA=$3
-DATA=$4
+SCIDT=$4
 
 #  -v option mounts the place where this command was run from as /tmp/evidence_extractor
-docker run -i -t -v $PWD:/tmp/scidt/ -v $DATA:/tmp/data -v $ES_DATA:/tmp/es_data -w=/tmp/scidt/ --rm -p ${JPORT}:8888 -p ${SPORT}:8787 scidt
+docker run -i -t -v $SCIDT:/tmp/scidt/ -v $PWD:/tmp/data -v $ES_DATA:/tmp/es_data -w=/tmp/scidt/ --rm -p ${JPORT}:8888 -p ${SPORT}:8787 scidt
